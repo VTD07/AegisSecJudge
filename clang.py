6cb09edcab5c64a44a9bb6efb6133466f9e5f843
VTD07 <alone24o61995@gmail.com>
2025-12-27 02:18:59 +0700

    fix for clang judge

--- clang.py
+++ clang.py
@@ -1,51 +1,59 @@
 import subprocess
 import os 
 
+result = []
 currpath = os.getcwd()
-test_folder = os.path.join(currpath, "test_folder\\TONGCSNMOD")
+
+test_file = os.path.join(currpath, "pending\\cpp\\1_1_1.cpp")
+# test_folder = os.path.join(currpath ,"test_folder\\1")
 
 def compile_cpp(file_path):
     exe_path = os.path.splitext(file_path)[0] + ".exe"
     status = subprocess.run(["g++", file_path,"-o", exe_path],capture_output=True,text=True)
 
     if status.returncode != 0:
-        print(f"Compilation error in {file_path}")
         return None
-
     return exe_path
 
-def detect_RTE_TLE_AC(file_path):
+def detect_RTE_TLE_AC(file_path,test_folder,test_id,time_limit):
+    
+    result = []
+    
     try:
         
-        for i in range(1,21):
+        for i in range(1,len(os.listdir(test_folder))+1):
             
             index=f"{i:02d}"
             
             input_folder_path = os.path.join(test_folder,f"Test{index}")
-            input_file_path=os.path.join(input_folder_path,f"TONGCSNMOD.inp")
-            output_file_path=os.path.join(input_folder_path,f"TONGCSNMOD.out")
-            
+            input_file_path=os.path.join(input_folder_path,f"{test_id}.inp")
+            output_file_path=os.path.join(input_folder_path,f"{test_id}.out")
+
             with open(input_file_path, 'r') as input_file, open(output_file_path, 'r') as output_file:
-                status = subprocess.run([file_path], input=input_file.read(), capture_output=True, text=True, timeout=1)
+                status = subprocess.run([file_path], input=input_file.read(), capture_output=True, text=True, timeout=time_limit)
                 if status.returncode != 0:
-                    # print(f"RTE on test case {index}")
-                    return "RTE"
+                    result.append(("RTE",i))
+                    return result 
                 else: 
                     if status.stdout.strip() != output_file.read().strip():
-                        # print(f"WA on test case {index}")
-                        return "WA"
+                        result.append(("WA",i))
+                    else: result.append(("AC",i))
 
     except subprocess.TimeoutExpired:
         # print(f"TLE on test case {index}")
-        return "TLE"
+        result.append(("TLE",i))
+            
+    return result 
     
-    return "AC"
     
-def complie(file_path): 
+def complie(file_path,test_name,time_limit): 
     exe_path = compile_cpp(file_path)
+    test_folder = os.path.join(currpath, "test_folder", f"{test_name}")
     if exe_path is None:
-        print(f"Error: Compilation failed for {file_path}")
-        exit(0)
+        return "CE", 0
     else:
-        result = detect_RTE_TLE_AC(exe_path)
-        print(f"Program resulted in: {result}")
+        status = detect_RTE_TLE_AC(exe_path,test_folder,test_name,time_limit)
+        for i in status: 
+            print(i)
+
+complie(test_file,1,1)
