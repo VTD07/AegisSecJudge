4c1f31011a05672f597471f0c5cfcd86fa4a2484
VTD07 <alone24o61995@gmail.com>
2025-12-30 08:52:22 +0700

    check for condition befor judge

---
+++ check_condition.py
@@ -0,0 +1,17 @@
+import folder_checking as fc 
+import subprocess 
+
+ness_folder = [
+    "test_folder", 
+    "pending", 
+    "db"
+]
+
+def check_folder(): 
+    return None 
+
+def check_cpp(): 
+    return None 
+
+def check_c(): 
+    return None 
